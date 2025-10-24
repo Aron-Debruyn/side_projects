f608a74aeae61aaceb0683e4a78f052aa9316945
Aron Debruyn <arondeklofio@gmail.com>
2025-10-24 23:32:12 +0200

    Fourth test

--- forest_cover.py
+++ forest_cover.py
@@ -4,12 +4,12 @@ Created on Tue Oct 21 23:47:12 2025
 
 @author: aron-
 """
-
+#TODO
 from sklearn.datasets import fetch_covtype
 import pandas as pd
- 
-# Get the dataset
-df = pd.DataFrame(fetch_covtype(as_frame=True))
- 
-# Print the first 5 rows of the dataframe
-print(df.head())
+import numpy as np
+
+array = fetch_covtype(as_frame=True).frame 
+# Add something here
+print(array)
+print(((array.columns)))
